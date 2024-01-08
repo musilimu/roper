97a1f43a488a92b55ac817ef76e1136b7bd4d9a1
musllim <muslimuwitondanishema@gmail.com>
2024-01-08 11:23:40 +0200

    configure third web

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { Web3Button } from "@thirdweb-dev/react";
 import "./App.css";
 
 function App() {
-  return <>hello world</>;
+  return (
+    <>
+      <Web3Button
+        contractAddress="{{contract_address}}"
+        action={async (contract) => contract.call("myFunctionName")}
+      >
+        Call myFunctionName from the connected wallet
+      </Web3Button>
+    </>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { ThirdwebProvider } from "@thirdweb-dev/react";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThirdwebProvider activeChain="ethereum" clientId="your-client-id">
+      <App />
+    </ThirdwebProvider>
+  </React.StrictMode>
+);
