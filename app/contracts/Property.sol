07eb0177b50b748c28796ac790bba4d2f34fc0fb
musllim <muslimuwitondanishema@gmail.com>
2024-01-26 11:41:35 +0200

    remove getter functions make valiable mapping public

--- app/contracts/Property.sol
+++ app/contracts/Property.sol
@@ -4,9 +4,6 @@ pragma solidity ^0.8.0;
 import "@thirdweb-dev/contracts/base/ERC721Base.sol";
 
 contract Property is ERC721Base {
-    uint256 propertiesSize;
-    uint256 propertyAddressessSize;
-
     constructor(
         address _defaultAdmin,
         string memory _name,
@@ -45,19 +42,11 @@ contract Property is ERC721Base {
         uint256 stars;
     }
 
-    mapping(uint256 => Lesson) lessons;
-    mapping(uint256 => mapping(address => Review)) lessonsReviews;
-    mapping(uint256 => mapping(uint256 => Exercise)) exercises;
+    mapping(uint256 => Lesson) public lessons;
+    mapping(uint256 => mapping(address => Review)) public lessonsReviews;
+    mapping(uint256 => mapping(uint256 => Exercise)) public exercises;
     uint256 public exercisesCount = 0;
     uint256 public lessonsCount = 0;
-
-    function getLessons() public view returns (Lesson[] memory) {
-        Lesson[] memory lessons_ = new Lesson[](lessonsCount);
-        for (uint256 i = 0; i < lessonsCount; i++) {
-            lessons_[i] = lessons[i];
-        }
-        return lessons_;
-    }
 
     event createLessonE(string notes, string name);
 
@@ -99,18 +88,6 @@ contract Property is ERC721Base {
         emit addExerciseE(lessonId, question, answerTexts, isCorrect);
     }
 
-    function getExercises(
-        uint256 lessonId
-    ) public view returns (Exercise[] memory) {
-        require(lessonId < lessonsCount, "Lesson does not exist");
-
-        Exercise[] memory lessonExercises = new Exercise[](exercisesCount);
-        for (uint256 i = 0; i < exercisesCount; i++) {
-            lessonExercises[i] = exercises[lessonId][i];
-        }
-        return lessonExercises;
-    }
-
     event addReviewE(uint256 lessonId, string message, uint256 stars);
 
     function addReview(
@@ -123,30 +100,5 @@ contract Property is ERC721Base {
 
         lessonsReviews[lessonId][msg.sender] = Review(message, stars);
         emit addReviewE(lessonId, message, stars);
-    }
-
-    function getReviews(
-        uint256 lessonId
-    ) public view returns (Review[] memory) {
-        require(lessonId < lessonsCount, "Lesson does not exist");
-
-        uint256 numReviews = 0;
-        address[] memory reviewers = new address[](lessonsCount);
-
-        for (uint256 i = 0; i < lessonsCount; i++) {
-            if (lessonsReviews[lessonId][msg.sender].stars > 0) {
-                reviewers[numReviews] = msg.sender;
-                numReviews++;
-            }
-        }
-
-        Review[] memory lessonReviews = new Review[](numReviews);
-
-        for (uint256 i = 0; i < numReviews; i++) {
-            address reviewer = reviewers[i];
-            lessonReviews[i] = lessonsReviews[lessonId][reviewer];
-        }
-
-        return lessonReviews;
     }
 }
--- client/src/components/Property.tsx
+++ client/src/components/Property.tsx
@@ -1,21 +1,34 @@
 import { Avatar, Badge, Box, Button, Card, Flex, Text } from "@radix-ui/themes";
 import { useContract, useContractRead } from "@thirdweb-dev/react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 export const Property = () => {
-  const { contract } = useContract(import.meta.env.VITE_CONTRACTADDRESS);
-  const { data: lessons, isLoading } = useContractRead(contract, "getLessons");
+  const { contract } = useContract(import.meta.env.VITE_CONTRACTADDRESS, "");
+  const [next, setnext] = useState(0);
+  const { data, isLoading } = useContractRead(contract, "lessons", [next]);
+  const { data: lessonsCount, isLoading: loadingLessonsCount } =
+    useContractRead(contract, "lessonsCount");
+  const [lessons, setLessons] = useState<
+    { creator: string; body: string; isPpublished: boolean; title: string }[]
+  >([]);
+
+  useEffect(() => {
+    if (isLoading || loadingLessonsCount || !data) return;
+    if (lessonsCount < next) return;
+    const [creator, body, isPpublished, title] = data;
+    setLessons([...lessons, { creator, body, isPpublished, title }]);
+    setnext(next + 1);
+  }, [isLoading, data, lessons, next, loadingLessonsCount, lessonsCount]);
 
   return (
     <>
       <h1>Lessons</h1>
-      {isLoading ? (
+      {isLoading || loadingLessonsCount ? (
         <h1>Loading ...</h1>
       ) : (
         <Flex my="4" gap="3" wrap="wrap" width="auto">
-          {(
-            lessons as [string, string, boolean, string, string][] | undefined
-          )?.map(([creator, body, isPpublished, title], i) => (
+          {lessons.map(({ creator, body, isPpublished, title }, i) => (
             <Card key={i} style={{ maxWidth: "500px" }}>
               <Flex gap="3">
                 <Avatar size="4" src={creator} fallback="T" />
@@ -23,7 +36,17 @@ export const Property = () => {
                   <Text mt="2" as="div" size="2" color="gray" weight="bold">
                     {title}
                   </Text>
-                  <Text mt="2" as="div" size="2" weight="light">
+                  <Text
+                    mt="2"
+                    as="div"
+                    size="2"
+                    weight="light"
+                    style={{
+                      overflow: "hidden",
+                      textOverflow: "ellipsis",
+                      whiteSpace: "nowrap",
+                    }}
+                  >
                     {body}
                   </Text>
                   <Text mt="2" as="div" size="2" color="cyan">
