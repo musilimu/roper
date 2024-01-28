eaa44f01a0b3f322b150a0555c0346ab0de242fa
musllim <muslimuwitondanishema@gmail.com>
2024-01-28 21:09:18 +0200

    add useLesson hook

---
+++ client/src/hooks/lesson.ts
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+import { Property } from "../types/Property";
+import { useProperty } from "../api/property";
+
+export function useLessons() {
+  const [next, setnext] = useState(0);
+  const [lessons, setLessons] = useState<Property[]>([]);
+  const {
+    data: { lessons: data, lessonsCount },
+    error,
+    isLoading,
+  } = useProperty(next);
+
+  useEffect(() => {
+    if (isLoading || !data) return;
+    if (lessonsCount < next) return;
+    const [creator, body, isPpublished, title] = data;
+    setLessons([...lessons, { creator, body, isPpublished, title }]);
+    setnext(next + 1);
+  }, [isLoading, data, lessons, next, lessonsCount]);
+
+  return { lessons, error, isLoading };
+}
