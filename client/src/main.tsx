b94814741c3c60166ac3e602fcb51f8dc7e7d4f2
musllim <muslimuwitondanishema@gmail.com>
2024-03-03 09:52:24 +0200

    add Error Boundary

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,26 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 import { Theme } from "@radix-ui/themes";
+import { ErrorBoundary } from "react-error-boundary";
+import App from "./App.tsx";
+
 import "@radix-ui/themes/styles.css";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThirdwebProvider
-      activeChain={import.meta.env.VITE_ACTIVECHAIN}
-      clientId={import.meta.env.VITE_CLIENTID}
-    >
-      <Theme
-        accentColor="crimson"
-        grayColor="sand"
-        radius="large"
-        scaling="95%"
+    <ErrorBoundary fallback={<p>⚠️Something went wrong</p>}>
+      <ThirdwebProvider
+        activeChain={import.meta.env.VITE_ACTIVECHAIN}
+        clientId={import.meta.env.VITE_CLIENTID}
       >
-        <App />
-      </Theme>
-    </ThirdwebProvider>
+        <Theme
+          accentColor="crimson"
+          grayColor="sand"
+          radius="large"
+          scaling="95%"
+        >
+          <App />
+        </Theme>
+      </ThirdwebProvider>
+    </ErrorBoundary>
   </React.StrictMode>
 );
 
